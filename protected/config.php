28435b5bdf2bae3629f066c46ff07262524da598
Albert Stepantsev <philfromfmf@mail.ru>
2015-01-15 20:28:29 +0300

    + first commit

---
+++ protected/config.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'db' => [
+        'default' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'dbname' => 'standard',
+            'user' => 'root',
+            'password' => '',
+        ]
+    ],
+    'auth' => [
+        'expire' => 31536000 // 1 year
+    ],
+    'mail' => [
+        'method' => 'php',
+    ],
+    'extensions' => [
+        'jquery' => [
+        ],
+        'bootstrap' => [
+        ],
+    ],
+];
