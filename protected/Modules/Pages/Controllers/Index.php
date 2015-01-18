e9c54e35bd37eaf10c5dae4b2eb60e17078173d2
Aleksey <aleksey.maksarov@gmail.com>
2015-01-18 03:46:31 +0300

    +Контроллер показа страницы по ее url, соответствующий шаблон и роут

---
+++ protected/Modules/Pages/Controllers/Index.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\Pages\Controllers;
+
+use App\Modules\Pages\Models\Page;
+use T4\Http\E404Exception;
+use T4\Mvc\Controller;
+
+class Index
+    extends Controller
+{
+
+    public function actionPageByUrl($url)
+    {
+        $page = Page::findByUrl($url);
+        $this->data->page = $page;
+        if (empty($this->data-page)) {
+            throw new E404Exception;
+        }
+    }
+
+} 
--- protected/routes.php
+++ protected/routes.php
@@ -2,4 +2,6 @@
 
 return [
     '/index' => '///',
+
+    '/pages/<1>' => '/Pages/Index/PageByUrl(url=<1>)',
 ];
