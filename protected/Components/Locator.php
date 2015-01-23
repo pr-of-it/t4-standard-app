b6d2b1ca6c793b3abc53651c76403dad9919cc61
Степанцев Альберт Сергеевич <astepantsev@corp.finam.ru>
2015-01-23 12:57:04 +0300

    + Locator class is added

--- protected/Components/Locator.php
+++ protected/Components/Locator.php
@@ -2,13 +2,18 @@
 
 namespace App\Components;
 
+use T4\Core\Session;
+
 class Locator
 {
 
     public static function getLocation()
     {
-        $app = \T4\Mvc\Application::getInstance();
-        return $app->extensions->sxgeo->getLocation($app->request->ip);
+        if (null == Session::get('location')) {
+            $app = \T4\Mvc\Application::getInstance();
+            Session::set('location', $app->extensions->sxgeo->getLocation($app->request->ip));
+        }
+        return Session::get('location');
     }
 
 }
