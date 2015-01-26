944bb84c6f14e93a9e61b098552609c6c0e49b9e
Aleksey <aleksey.maksarov@gmail.com>
2015-01-26 11:54:43 +0300

    Maps Module development

--- protected/Migrations/m_1421913302_createMaps.php
+++ protected/Migrations/m_1421913302_createMaps.php
@@ -13,37 +13,37 @@ class m_1421913302_createMaps
     {
         $this->createTable('maps', [
             'title' => ['type' => 'string'],
-            'latitude' => ['type' => 'float', 'dimension' => '8,6'],
-            'longitude' => ['type' => 'float', 'dimension' => '8,6'],
+            'latitude' => ['type' => 'float', 'dimension' => '11,8'],
+            'longitude' => ['type' => 'float', 'dimension' => '11,8'],
             'width' => ['type' => 'integer'],
             'height' => ['type' => 'integer'],
             'zoom' => ['type' => 'integer'],
             'layer' => ['type' => 'string', 'default' => 'map'],
-            'ptLatitude' => ['type' => 'float', 'dimension' => '8,6'],
-            'ptLongitude' => ['type' => 'float', 'dimension' => '8,6'],
+            'ptLatitude' => ['type' => 'float', 'dimension' => '11,8'],
+            'ptLongitude' => ['type' => 'float', 'dimension' => '11,8'],
             'ptStyle' => ['type' => 'string', 'default' => 'flag'],
         ]);
 
         $map = new Map();
         $map->title = 'Карта Москвы';
-        $map->latitude = 55.753632;
-        $map->longitude = 37.620071;
+        $map->latitude = 55.75358253;
+        $map->longitude = 37.62091000;
         $map->width = 450;
         $map->height = 450;
         $map->zoom = 13;
-        $map->ptLatitude = 55.753632;
-        $map->ptLongitude = 37.620071;
+        $map->ptLatitude = 55.75358253;
+        $map->ptLongitude = 37.62091000;
         $map->save();
 
         $map1 = new Map();
         $map1->title = 'Уменьшенная карта Москвы';
-        $map1->latitude = 55.753632;
-        $map1->longitude = 37.620071;
+        $map1->latitude = 55.75358253;
+        $map1->longitude = 37.62091000;
         $map1->width = 200;
         $map1->height = 200;
         $map1->zoom = 15;
-        $map1->ptLatitude = 55.753632;
-        $map1->ptLongitude = 37.620071;
+        $map1->ptLatitude = 55.75358253;
+        $map1->ptLongitude = 37.62091000;
         $map1->save();
     }
 
--- protected/Modules/Maps/Templates/assets/js/maps.js
+++ protected/Modules/Maps/Templates/assets/js/maps.js
@@ -2,21 +2,49 @@ var myMap, myPlacemark, coords;
 
 ymaps.ready(init);
 
-function init () {
+function init() {
 
     myMap = new ymaps.Map('map', {
-        center: [55.753632, 37.620071],
+        center: [55.75358253, 37.62091000],
         zoom: 12
     });
 
     myMap.controls.remove('typeSelector');
     myMap.controls.remove('trafficControl');
 
-    coords = [55.753632, 37.620071];
+    coords = myMap.getCenter();
 
-    myPlacemark = new ymaps.Placemark([55.753632, 37.620071],{},{preset: 'islands#redDotIcon', draggable: true});
+    myPlacemark = new ymaps.Placemark([coords[0], coords[1]],{},{preset: 'islands#redDotIcon', draggable: true});
 
     myMap.geoObjects.add(myPlacemark);
+
+    //установка начальных значений
+    var mapWidth = $('#width');
+    var mapHeight = $('#height');
+    var mapContainer = myMap.container.getElement();
+    var latitude = $('#latitude');
+    var longitude = $('#longitude');
+    var ptLatitude = $('#ptLatitude');
+    var ptLongitude = $('#ptLongitude');
+
+    if(mapWidth.val() && mapHeight.val()) {
+        mapContainer.style.width = mapWidth.val() + 'px';
+        mapContainer.style.height = mapHeight.val() + 'px';
+        $('#map').width(mapWidth.val()).height(mapHeight.val());
+        myMap.container.fitToViewport();
+        myMap.setZoom($('#zoom').val());
+        myMap.setCenter([latitude.val(), longitude.val()]);
+        myPlacemark.geometry.setCoordinates([ptLatitude.val(), ptLongitude.val()]);
+        coords = [ptLatitude.val(), ptLongitude.val()];
+    } else {
+        mapWidth.val($('#map').width());
+        mapHeight.val($('#map').height());
+        latitude.val(coords[0]);
+        longitude.val(coords[1]);
+        $('#zoom').val(myMap.getZoom());
+        ptLatitude.val(coords[0]);
+        ptLongitude.val(coords[1]);
+    }
 
     myPlacemark.events.add("dragend", function (e) {
         coords = this.geometry.getCoordinates();
@@ -36,15 +64,28 @@ function init () {
             savecoordinats();
         }
     });
+
+    //изменение размеров карты
+    mapWidth.change(function () {
+        $('#map').width($(this).val());
+        myMap.container.fitToViewport();
+    });
+
+    mapHeight.change(function () {
+        $('#map').height($(this).val());
+        myMap.container.fitToViewport();
+    });
+
 }
 
-function savecoordinats (){
+function savecoordinats(){
     myPlacemark.geometry.setCoordinates(coords);
-    $('#ptLatitude').val(coords[0].toFixed(6));
-    $('#ptLongitude').val(coords[1].toFixed(6));
+    var ptCoords = myPlacemark.geometry.getCoordinates();
+    $('#ptLatitude').val(parseFloat(ptCoords[0]).toFixed(8));
+    $('#ptLongitude').val(parseFloat(ptCoords[1]).toFixed(8));
     var zoom = myMap.getZoom();
     $('#zoom').val(zoom);
     var center = myMap.getCenter();
-    $('#latitude').val(center[0].toFixed(6));
-    $('#longitude').val(center[1].toFixed(6));
+    $('#latitude').val(center[0].toFixed(8));
+    $('#longitude').val(center[1].toFixed(8));
 }
