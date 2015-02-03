b8b42331d27c03a8f462285899cfca6507107c67
Aleksey <aleksey.maksarov@gmail.com>
2015-02-03 07:09:27 +0300

    migration_core.php is added

---
+++ protected/Migrations/m_1422936021_migration_core.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Migrations;
+
+use T4\Orm\Migration;
+
+class m_1422936021_migration_core
+    extends Migration
+{
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+
+}
