70fb1021a5ed86927b97ac4c73f57d60069aef43
Степанцев Альберт Сергеевич <astepantsev@corp.finam.ru>
2015-02-04 14:29:25 +0300

    + Модуль новостей

---
+++ protected/Modules/News/Models/Story.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Modules\News\Models;
+
+use T4\Core\Exception;
+use T4\Fs\Helpers;
+use T4\Http\Uploader;
+use T4\Mvc\Application;
+use T4\Orm\Model;
+
+class Story
+    extends Model
+{
+    static protected $schema = [
+        'table' => 'news_stories',
+        'columns' => [
+            'title' => ['type'=>'string'],
+            'published' => ['type'=>'datetime'],
+            'image' => ['type'=>'string', 'default'=>''],
+            'lead' => ['type'=>'text'],
+            'text' => ['type'=>'text'],
+        ],
+        'relations' => [
+            'topic' => ['type'=>self::BELONGS_TO, 'model'=>Topic::class],
+        ]
+    ];
+
+    public function uploadImage($formFieldName)
+    {
+        $request = Application::getInstance()->request;
+        if (!$request->existsFilesData() || !$request->isUploaded($formFieldName) || $request->isUploadedArray($formFieldName))
+            return $this;
+
+        try {
+            $uploader = new Uploader($formFieldName);
+            $uploader->setPath('/public/news/stories/images');
+            $image = $uploader();
+            if ($this->image)
+                $this->deleteImage();
+            $this->image = $image;
+        } catch (Exception $e) {
+            $this->image = null;
+        }
+        return $this;
+    }
+
+    public function beforeDelete()
+    {
+        $this->deleteImage();
+        return parent::beforeDelete();
+    }
+
+    public function deleteImage()
+    {
+        if ($this->image) {
+            try {
+                $this->image = '';
+                Helpers::removeFile(ROOT_PATH_PUBLIC . $this->image);
+            } catch (\T4\Fs\Exception $e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
