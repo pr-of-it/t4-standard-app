a420baa09a1333bf71fdf4fbc49be48b52b95d33
Alexey <gembux2012@gmail.com>
2015-02-04 12:27:02 +0300

    модуль меню

---
+++ protected/Modules/Menu/Models/Menu.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Вера
+ * Date: 03.02.2015
+ * Time: 19:40
+ */
+
+namespace App\Modules\Menu\Models;
+
+
+use T4\Orm\Model;
+
+class Menu
+      extends Model
+{
+    static protected $schema = [
+        'table' => 'menu',
+        'columns' => [
+            'title' => [
+                'type' => 'string',
+            ],
+            'url' => [
+                'type' => 'string',
+            ],
+        ],
+    ];
+
+    static protected $extensions = ['tree'];
+
+}
