8b1c4a9e10c878675c4523fde309c82a185cfcc8
Alexey <gembux2012@gmail.com>
2015-02-05 08:04:46 +0300

    модуль меню

--- protected/Modules/Menu/Controllers/Admin.php
+++ protected/Modules/Menu/Controllers/Admin.php
@@ -9,80 +9,105 @@ use App\Components\Admin\Controller;
 class Admin
     extends Controller
 {
+    /*
+     protected function access($action)
+     {
+         return !empty($this->app->user) && $this->app->user->hasRole('admin');
+     }
+ */
     public function actionDefault()
     {
-        $this->app->extensions->jstree->init();
-        $this->data->items = MenuModel::findAllTree();
+        $this->data->items = Page::findAllTree();
     }
+
 
     public function actionEdit($id=null, $parent=null)
     {
+        $this->app->extensions->ckeditor->init();
+        $this->app->extensions->ckfinder->init();
+
         if (null === $id || 'new' == $id) {
-            $this->data->item = new MenuModel();
+            $this->data->item = new Page();
             if (null !== $parent) {
                 $this->data->item->parent = $parent;
             }
         } else {
-            $this->data->item = MenuModel::findByPK($id);
+            $this->data->item = Page::findByPK($id);
         }
     }
 
-    public function actionSave()
+    public function actionSave($redirect = 0)
     {
-        if (!empty($_POST[MenuModel::PK])) {
-            $item = MenuModel::findByPK($_POST[MenuModel::PK]);
+        if (!empty($_POST[Page::PK])) {
+            $item = Page::findByPK($_POST[Page::PK]);
         } else {
-            $item = new MenuModel();
+            $item = new Page();
         }
         $item
             ->fill($_POST)
+            ->uploadFiles('files')
             ->save();
         if ($item->wasNew()) {
             $item->moveToFirstPosition();
         }
-        $this->redirect('/admin/menu/');
+        if ($redirect) {
+            $this->redirect('/pages/' . $item->url . '.html');
+        } else {
+            $this->redirect('/admin/pages/');        }
+
     }
 
     public function actionDelete($id)
     {
-        $item = MenuModel::findByPK($id);
+        $item = Page::findByPK($id);
         if ($item)
             $item->delete();
-        $this->redirect('/admin/menu/');
+        $this->redirect('/admin/pages/');
+    }
+
+    public function actionDeleteFile($id)
+    {
+        $item = File::findByPK($id);
+        if ($item) {
+            $item->delete();
+            $this->data->result = true;
+        } else {
+            $this->data->result = false;
+        }
     }
 
     public function actionUp($id)
     {
-        $item = MenuModel::findByPK($id);
+        $item = Page::findByPK($id);
         if (empty($item))
-            $this->redirect('/admin/menu/');
+            $this->redirect('/admin/pages/');
         $sibling = $item->getPrevSibling();
         if (!empty($sibling)) {
             $item->insertBefore($sibling);
         }
-        $this->redirect('/admin/menu/');
+        $this->redirect('/admin/pages/');
     }
 
     public function actionDown($id)
     {
-        $item = MenuModel::findByPK($id);
+        $item = Page::findByPK($id);
         if (empty($item))
-            $this->redirect('/admin/menu/');
+            $this->redirect('/admin/pages/');
         $sibling = $item->getNextSibling();
         if (!empty($sibling)) {
             $item->insertAfter($sibling);
         }
-        $this->redirect('/admin/menu/');
+        $this->redirect('/admin/pages/');
     }
 
     public function actionMoveBefore($id, $to)
     {
         try {
-            $item = MenuModel::findByPK($id);
+            $item = Page::findByPK($id);
             if (empty($item)) {
                 throw new Exception('Source element does not exist');
             }
-            $destination = MenuModel::findByPK($to);
+            $destination = Page::findByPK($to);
             if (empty($destination)) {
                 throw new Exception('Destination element does not exist');
             }
@@ -97,11 +122,11 @@ class Admin
     public function actionMoveAfter($id, $to)
     {
         try {
-            $item = MenuModel::findByPK($id);
+            $item = Page::findByPK($id);
             if (empty($item)) {
                 throw new Exception('Source element does not exist');
             }
-            $destination = MenuModel::findByPK($to);
+            $destination = Page::findByPK($to);
             if (empty($destination)) {
                 throw new Exception('Destination element does not exist');
             }
