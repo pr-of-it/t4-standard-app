b0b9f09300a4a3c29f4ba51e8072271d084440d7
Alexey <gembux2012@gmail.com>
2015-02-05 08:54:24 +0300

    модули

--- protected/Modules/Pages/Models/Page.php
+++ protected/Modules/Pages/Models/Page.php
@@ -9,9 +9,47 @@ class Page
 {
     static protected $schema = [
         'columns' => [
-            'url' => ['type' => 'string'],
-            'title' => ['type' => 'string'],
-            'text' => ['type' => 'text'],
+            'title' => [
+                'type' => 'string',
+            ],
+            'url' => [
+                'type' => 'string',
+            ],
+            'template' => [
+                'type' => 'string',
+            ],
+            'text' => [
+                'type' => 'text',
+                'length' => 'big',
+            ],
+            'order' => [
+                'type' => 'int',
+                'default' => 0
+            ],
+            'file' => [
+                'type' => 'string',
+            ],
+        ],
+        'relations' => [
+            'files' => ['type' => self::HAS_MANY, 'model' => '\App\Modules\Pages\Models\File']
         ],
     ];
+
+    static protected $extensions = ['tree'];
+
+    public function getBreadCrumbs()
+    {
+        $ret = new Collection;
+        foreach ($this->findAllParents() as $i => $parent) {
+            if (0==$i)
+                continue;
+            $p = new Std;
+            $p->url = $parent->url;
+            $p->title = $parent->title;
+            $ret[] = $p;
+        }
+        return $ret;
+    }
+
+
 }
