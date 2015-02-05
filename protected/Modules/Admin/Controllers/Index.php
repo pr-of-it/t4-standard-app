90c98f11640ee2e59714808761c2025768ab5df2
Степанцев Альберт Сергеевич <astepantsev@corp.finam.ru>
2015-02-05 17:08:05 +0300

    + Создание админ-панели

--- protected/Modules/Admin/Controllers/Index.php
+++ protected/Modules/Admin/Controllers/Index.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Admin\Controllers;
 
 use App\Components\Admin\Controller;
+use T4\Core\Exception;
 use T4\Http\E404Exception;
 use T4\Mvc\Route;
 use T4\Mvc\Router;
@@ -17,14 +18,15 @@ class Index
 
     public function actionModule($module, $action=Router::DEFAULT_ACTION)
     {
-        $controllerClass = '\\App\\Modules\\' . ucfirst($module) .'\\Controllers\\Admin';
-        if (!class_exists($controllerClass)) {
-            throw new E404Exception('Не найден админ-контроллер в модуле ' . $module);
-        }
+        try {
 
-        $route = new Route('/' . ucfirst($module) . '/Admin/' . ucfirst($action));
-        $this->app->runRoute($route);
-        exit;
+            $route = new Route('/' . ucfirst($module) . '/Admin/' . ucfirst($action));
+            $this->app->runRoute($route);
+            exit;
+
+        } catch (Exception $e) {
+            throw new E404Exception('Ошибка админ-панели в модуле ' . $module . ': ' . $e->getMessage());
+        }
     }
 
 }
