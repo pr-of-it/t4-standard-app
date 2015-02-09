d6d68452698dbabc4b7f379568fd1380fd42eb1d
Степанцев Альберт Сергеевич <astepantsev@corp.finam.ru>
2015-02-09 16:30:01 +0300

    + Админ-панель новостей

---
+++ protected/Modules/News/Controllers/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\News\Controllers;
+
+use App\Modules\News\Models\Story;
+use T4\Http\E404Exception;
+use T4\Mvc\Controller;
+
+class Index
+    extends Controller
+{
+
+    public function actionOne($id)
+    {
+        $this->data->item = Story::findByPK($id);
+        if (empty($this->data->item)) {
+            throw new E404Exception('Новость не найдена');
+        }
+    }
+
+}
