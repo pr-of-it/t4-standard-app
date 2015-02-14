9d39d9021117328bd78c9260b470d5693d9c4bbd
vera23 <veramir10@gmail.com>
2015-02-14 22:40:39 +0400

    modul menu works

--- protected/Modules/Menu/Controllers/Index.php
+++ protected/Modules/Menu/Controllers/Index.php
@@ -5,9 +5,11 @@ namespace App\Modules\Menu\Controllers;
 
 use App\Components\Admin\Controller;
 
-class Index extends Controller {
-    public function actionMenu(){
-
+class Index extends Controller
+{
+    public function actionMenu()
+    {
+        $this->data->items = \App\Modules\Menu\Models\Menu::findAllTree();
     }
 
 }
