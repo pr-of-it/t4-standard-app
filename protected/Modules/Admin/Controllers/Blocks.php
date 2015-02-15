d7b11558fd1ccc95324677efbbd6d89403c0a140
Albert Stepantsev <philfromfmf@mail.ru>
2015-02-15 21:00:10 +0300

    ! Админ-панель блоков

--- protected/Modules/Admin/Controllers/Blocks.php
+++ protected/Modules/Admin/Controllers/Blocks.php
@@ -5,6 +5,7 @@ namespace App\Modules\Admin\Controllers;
 use App\Components\Admin\Controller;
 use App\Models\Block;
 use T4\Core\Collection;
+use T4\Dbal\QueryBuilder;
 
 class Blocks
     extends Controller
@@ -19,6 +20,36 @@ class Blocks
         $this->data->blocksInstalled = new Collection();
         foreach ($installed as $block) {
             $this->data->blocksInstalled[$block->section][] = $block;
+        }
+    }
+
+    public function actionSetupBlock($sectionId, $blockPath)
+    {
+        $query = new QueryBuilder();
+        $query
+            ->select('MAX(' . Block::getDbDriver()->quoteName('order') . ')')
+            ->from(Block::getTableName())
+            ->where('section=:section')
+            ->params([':section' => $sectionId]);
+        $maxOrder = (int)$this->app->db->default->query($query)->fetchScalar();
+
+        $block = new Block();
+        $block->section = $sectionId;
+        $block->path = $blockPath;
+        $block->template = '';
+
+        $params = [];
+        foreach ($this->app->config->blocks->{$blockPath}->options as $optionName => $options) {
+            $params[$optionName] = $options->default;
+        }
+        $block->options = json_encode($params);
+        $block->order = $maxOrder + 10;
+
+        if (false !== $block->save()) {
+            $this->data->id = $block->getPK();
+            $this->data->result = true;
+        } else {
+            $this->data->result = false;
         }
     }
 
