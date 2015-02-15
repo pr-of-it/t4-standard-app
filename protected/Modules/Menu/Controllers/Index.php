94f83d249a1dafef305d124942f2ce18c3719461
Albert Stepantsev <philfromfmf@mail.ru>
2015-02-15 18:25:24 +0300

    ! Интеграция меню в лэйаут

--- protected/Modules/Menu/Controllers/Index.php
+++ protected/Modules/Menu/Controllers/Index.php
@@ -3,13 +3,14 @@
 
 namespace App\Modules\Menu\Controllers;
 
+use App\Modules\Menu\Models\Menu as MenuModel;
 use T4\Mvc\Controller;
 
 class Index extends Controller
 {
-    public function actionMenu()
+    public function actionDefault()
     {
-        $this->data->items = \App\Modules\Menu\Models\Menu::findAllTree();
+        $this->data->items = MenuModel::findAllTree();
     }
 
 }
