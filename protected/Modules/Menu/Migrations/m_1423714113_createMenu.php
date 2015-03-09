b4a2a06f14e723a74fffd5a60c433b9ad21781cd
Albert Stepantsev <philfromfmf@mail.ru>
2015-03-09 18:37:56 +0300

    - лишняя ссылка в стартовом меню

--- protected/Modules/Menu/Migrations/m_1423714113_createMenu.php
+++ protected/Modules/Menu/Migrations/m_1423714113_createMenu.php
@@ -26,30 +26,21 @@ class m_1423714113_createMenu
         );
 
         $this->insert('menu', [
-                'title'=>'Админ-панель',
-                'url' =>'/admin',
-                '__lft' =>'1',
-                '__rgt'=>'2',
-                '__lvl'=>'0',
-                '__prt' =>'0',
-            ]
-            );
-        $this->insert('menu', [
-                'title'=>'Контакты',
-                'url' =>'/contact',
-                '__lft' =>'3',
-                '__rgt'=>'4',
-                '__lvl'=>'0',
-                '__prt' =>'0',
+                'title' => 'Контакты',
+                'url' => '/contact',
+                '__lft' => '1',
+                '__rgt' => '2',
+                '__lvl' => '0',
+                '__prt' => '0',
             ]
         );
         $this->insert('menu', [
-                'title'=>'Новости',
-                'url' =>'/news',
-                '__lft' =>'5',
-                '__rgt'=>'6',
-                '__lvl'=>'0',
-                '__prt' =>'0',
+                'title' => 'Новости',
+                'url' => '/news',
+                '__lft' => '3',
+                '__rgt' => '4',
+                '__lvl' => '0',
+                '__prt' => '0',
             ]
         );
 
